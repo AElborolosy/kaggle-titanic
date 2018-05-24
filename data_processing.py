918a1233a60d7264547541020ad70d5de6f15c7c
AElborolosy <ahmed.elborolosy24@gmail.com>
2018-05-24 19:44:32 -0400

    Completed Data Processing

--- data_processing.py
+++ data_processing.py
@@ -6,13 +6,12 @@ and normalizing numeric data
 import numpy as np
 import pandas as pd
 from sklearn.feature_selection import SelectKBest
-from sklearn.preprocessing import normalize
-
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import Imputer
 
 # Given a Series object of numeric data, normalize it
 def process_data(df):
     for col in df.columns:
-        print(col, df[col].dtype)
         if df[col].dtype == 'float64' or df[col].dtype == 'int64':
             df[col] = normalize_numeric_data(df[col])
         else:
@@ -22,16 +21,22 @@ def process_data(df):
 
 # Reshape and normalize numeric data
 def normalize_numeric_data(feature):
-    feature.dropna(inplace=True)
-    temp_feature = np.array(feature.values)
-    reshaped_feature = temp_feature.reshape(-1, 1)
-    normalized_feature = normalize(reshaped_feature, axis=1, return_norm=True)
-    final_feature = pd.Series(normalized_feature)
-    return final_feature
+    # Reshape data for imputing and normalizing
+    feature = feature.values.reshape(-1, 1)
+
+    # Replace null values with the median
+    imp = Imputer(missing_values='NaN', strategy='median', axis=0)
+    feature = imp.fit_transform(feature)
+
+    # Normalize the data
+    scaler = StandardScaler()
+    feature = scaler.fit_transform(feature)
+    return feature
 
 
 # Encode and replace values in categorical data
 def encode_categorical_data(feature):
+    # Store list of unique values and replace them accordingly
     values = [val for val in feature.unique()]
     encoding = list(range(0, len(values)))
     feature.replace(to_replace=values, value=encoding, inplace=True)
@@ -43,12 +48,17 @@ def main():
     file_path = 'train.csv'
     data = pd.read_csv('train.csv', index_col = 'PassengerId')
     processed_data = process_data(data.drop(['Survived', 'Name', 'Cabin'], axis=1))
-    print(processed_data.head(3))
 
     # Score the features based on univariate predictive power
-    selector = SelectKBest(k='all', score_func='f_classif')
-    # selector.fit_transform(data.drop(['Survived'], axis=1), data['Survived'])
-    # print(selector.scores_)
+    selector = SelectKBest(k=3)
+    selector.fit(processed_data, data['Survived'])
+    scores = selector.scores_
+    for i, col in enumerate(processed_data):
+        print(col, scores[i])
+
+    x_new = selector.transform(processed_data)
+    x_new_df = pd.DataFrame(x_new)
+    print(x_new_df.head(3))
     return
 
 
